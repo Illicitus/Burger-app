627062b1efced414fe83103bacd945a90d32f06f
Illicitus <suenoyactuar@gmail.com>
2018-10-07 12:53:34 +0300

    Fix minor bugs.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -87,6 +87,7 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
+                validation: {},
                 valid: true
             }
         },
@@ -99,7 +100,7 @@ class ContactData extends Component {
         this.setState( { loading: true } );
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm) {
-            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
         }
         const order = {
             ingredients: this.props.ingredients,
